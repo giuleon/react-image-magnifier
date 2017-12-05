6119f0f5580981f62ae4d6e1d3a35d3e718e4ea5
Giuliano De Luca <admine73379@hugoboss.com>
2017-12-05 15:41:03 +0100

    updated app container

--- app/src/containers/App.tsx
+++ app/src/containers/App.tsx
@@ -5,24 +5,22 @@ import ContainerDimensions from "react-container-dimensions";
 export default class App extends React.Component<{}, {}> {
     public render() {
         return (
-            <ContainerDimensions>
-            { ({ width, height }) =>
+            <div className="app-container">
                 <ImageMagnifierLens
                     image={{
-                        src: "https://raw.githubusercontent.com/giuleon/react-image-magnifier/master/src/img/world-map-small.jpg",
-                        width: width,
-                        height: height
+                        src: "https://raw.githubusercontent.com/giuleon/react-image-magnifier/master/app/src/img/world-map-small.jpg",
+                        width: 400,
+                        height: 300
                     }}
                     zoomImage={{
-                        src: "https://raw.githubusercontent.com/giuleon/react-image-magnifier/master/src/img/world-map-large.jpg",
-                        width: (width * 4),
-                        height: (height * 4)
+                        src: "https://raw.githubusercontent.com/giuleon/react-image-magnifier/master/app/src/img/world-map-large.jpg",
+                        width: 1600,
+                        height: 1200
                     }}
                     cursorOffset={{ x: 80, y: -80 }}
                     size={ 200 }
                 />
-            }
-            </ContainerDimensions>
+            </div>
         );
     }
 }
